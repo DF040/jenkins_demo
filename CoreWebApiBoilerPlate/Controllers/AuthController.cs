fc0dc3cd5961194c7a9a1dbe3d6eaaea7371a854
Mak Thevar <mak.thevar11@gmail.com>
2022-05-23 12:57:34 +0530

    Adding the project files

---
+++ CoreWebApiBoilerPlate/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using CoreWebApiBoilerPlate.Controllers.Base;
+using CoreWebApiBoilerPlate.Infrastructure.Auth;
+using CoreWebApiBoilerPlate.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApiBoilerPlate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ApiControllerBase
+    {
+        private readonly IJWTAuth jWTAuth;
+
+        public AuthController(IJWTAuth jWTAuth)
+        {
+            this.jWTAuth = jWTAuth;
+        }
+
+        /// <summary>
+        /// Generate JWT token by providing the appropriate login credentials.
+        /// </summary>
+        /// <param name="login">LoginModel</param>
+        /// <returns></returns>
+        [HttpPost("login")]
+        [ProducesResponseType(200,Type = typeof(TokenResponseModel))]
+        [ProducesResponseType(400,Type = typeof(StatusCodeResult))]
+        public async Task<IActionResult> Login(LoginModel login)
+        {
+            var user = await jWTAuth.Authenticate(login.UserName, login.Password);
+            if (user == null)
+                return await CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, new Errors("Login Failed", "Invalid username or password."));
+            var token = jWTAuth.GenerateToken(user);
+            return await CreateSuccessResponse(token);
+        }
+    }
+}
---
+++ CoreWebApiBoilerPlate/Controllers/UsersController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HelpDeskBBQN.Infrastructure.Data;
+using CoreWebApiBoilerPlate.Controllers.Base;
+using CoreWebApiBoilerPlate.Entity;
+using Microsoft.AspNetCore.Authorization;
+using CoreWebApiBoilerPlate.Infrastructure.Data.Repository.Interfaces;
+using CoreWebApiBoilerPlate.Models;
+using AutoMapper;
+using AWSFileUploaderWithImageCompression.Interfaces;
+using AWSFileUploaderWithImageCompression.Classes;
+using Amazon;
+using Amazon.S3.Model;
+using AWSFileUploaderWithImageCompression.Classes.Models;
+
+namespace CoreWebApiBoilerPlate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ApiControllerBase
+    {
+        private readonly IRepositoryWrapper _context;
+        private readonly IMapper mapper;
+        public UsersController(IRepositoryWrapper context, IMapper mapper)
+        {
+            _context = context;
+            this.mapper = mapper;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var result =  await _context.UserRepository.GetAllAsync();
+            if (result.Any())
+                return await CreateSuccessResponse(result);
+            return await DataNotFound();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _context.UserRepository.GetByIdAsync(id);
+
+            if (user == null)
+            {
+                return await DataNotFound();
+            }
+
+            return await CreateSuccessResponse(user);
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.UserId)
+            {
+                return await CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, new Models.Errors("Error","UserId not found"));
+            }
+
+            _context.UserRepository.UpdateAsync(user);
+            await _context.Save();
+            return await CreateSuccessResponse("User updated successfully");
+        }
+
+        /// <summary>
+        /// Register a new User
+        /// </summary>
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> PostUser(UserRequestModel userReqModel)
+        {
+            var user = mapper.Map<User>(userReqModel);
+            var result = await _context.UserRepository.AddAsync(user);
+            await _context.Save();
+            return await CreateSuccessResponse(user);
+        }
+    }
+}
